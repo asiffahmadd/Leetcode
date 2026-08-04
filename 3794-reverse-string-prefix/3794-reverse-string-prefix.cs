d3051996e79d8088b104b26ab52b21869eac8359
Asif Ahmad <110142464+asiffahmadd@users.noreply.github.com>
2026-08-04 15:40:11 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.93%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public string ReversePrefix(string s, int k) {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr, 0, k);
+        return new string(arr);
+    }
+}
