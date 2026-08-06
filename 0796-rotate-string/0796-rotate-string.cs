f1227274765ce47d500ebec6f26c9f9d04eb155d
Asif Ahmad <110142464+asiffahmadd@users.noreply.github.com>
2026-08-06 18:38:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (25.12%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cs
+++ 0796-rotate-string/0796-rotate-string.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public bool RotateString(string s, string goal) {
-        if (s.Length != goal.Length) return false;
-        return (s + s).Contains(goal);
+        return s.Length == goal.Length && (s + s).Contains(goal);
     }
 }
