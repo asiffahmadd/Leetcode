c177b819e589cd09255e3ffab2c3cf0288f988e1
Asif Ahmad <110142464+asiffahmadd@users.noreply.github.com>
2026-08-15 11:11:53 +0530

    Time: 3 ms (87.5%), Space: 81.1 MB (62.5%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cs
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cs
@@ -4,21 +4,9 @@ public class Solution
     {
         int xor = 0;
         bool hasNonZero = false;
-
-        foreach (int num in nums)
-        {
-            xor ^= num;
-
-            if (num != 0)
-                hasNonZero = true;
-        }
-
-        if (xor != 0)
-            return nums.Length;
-
-        if (hasNonZero)
-            return nums.Length - 1;
-
+        foreach (int num in nums){ xor ^= num; if (num != 0) hasNonZero = true;}
+        if (xor != 0)return nums.Length;
+        if (hasNonZero)return nums.Length - 1;
         return 0;
     }
 }
