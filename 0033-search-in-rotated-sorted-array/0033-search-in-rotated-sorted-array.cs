de06803b056f134226f17e6a8eb0154718fef1ad
Asif Ahmad <110142464+asiffahmadd@users.noreply.github.com>
2026-09-21 22:50:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (70.16%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cs
@@ -0,0 +1,46 @@
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            // Target mil gaya
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half sorted hai
+            if (nums[left] <= nums[mid])
+            {
+                // Target left sorted half me hai
+                if (nums[left] <= target && target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            // Right half sorted hai
+            else
+            {
+                // Target right sorted half me hai
+                if (nums[mid] < target && target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
